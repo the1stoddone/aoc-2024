0a8fe7716ffca7b36337c43400a6516e7f1ff3a6
the1stoddone <96317147+the1stoddone@users.noreply.github.com>
2024-12-09 15:42:40 -0500

    Working. need to figure out parameter type mismatch

--- 02/SolutionDay2.ps1
+++ 02/SolutionDay2.ps1
@@ -1,22 +1,13 @@
-$puzzleInput = Get-Content .\originalinput.txt
-
-# For part 1, the criteria for any given line is "must be either all desc or asc" && "diff between 2 values must be 1 .. 3"
-$goodReport = 0
-$goodDampenedReport = 0
-
-foreach($report in $puzzleInput)
-{
-  # Yes the constant casting is sorta silly, but PS gets a bit weird about how it handles objects and arrays so I'd rather cast the
-  ## original over and over to have some certainty I'm referencing the correct thing than find and figure some esoteric
-  ## "Oh PS jsut makes a pointer in this case and you're not actually dealing with a new object" or some such
+function IsReportSafe {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $report
+  )
   
-  # Initializing return value; again, starting with good assumption for ease of short circuit
-  $isAGoodReport = $true
-  $DampenedCounter = 0
-  
-  # Each level is a series of digits separated by white space so just splitting then walking through the list
+  $isAGoodReport = $true  
   $reportValues = $report -split '\s+'
-
+  
   # We need to track asc vs desc, so we'll just store whether the first is smaller than the second  
   $isAscending = [int]$reportValues[0] -lt [int]$reportValues[1]
   
@@ -34,38 +25,74 @@ foreach($report in $puzzleInput)
     }
   }
 
-  if($isAGoodReport)
+  $isAGoodReport
+}
+
+function IsReportSafeWithDampening {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $report,
+    [Parameter(Mandatory=$false)]
+    [bool]
+    $wasDampened = $false
+  )
+
+  $isAGoodReport = $true  
+  # Turns out the arrays PS makes by default are fixed size. So we're going to use the fresh Generic.List
+  # So what we're going to do
+  $reportValues = $report -split '\s+'
+   
+  # We need to track asc vs desc, so we'll just store whether the first is smaller than the second  
+  $isAscending = [int]$reportValues[0] -lt [int]$reportValues[1]
+  
+  # Yes we're rechecking whether the first 2 values are doing the thing we used them to determine, but I didn't want to make counters
+  ## and crap for a while and order matters so can't foreach where my brain tells me the enumerator returned is not guaranteed to be sorted
+  for ($i = 0; ($i -lt $reportValues.Count - 1) -and $isAGoodReport; $i++) 
+  {    
+    if($isAscending)
+    {   
+        $isAGoodReport = ( ( [int]$reportValues[$i] -lt [int]$reportValues[$i+1] ) -and ( ( ( [int]$reportValues[$i+1] - [int]$reportValues[$i] ) -ge 1 ) -and ( ( [int]$reportValues[$i+1] - [int]$reportValues[$i] ) -le 3 ) ) )
+        # Write-Host "Ascending. Current offset $i . Values $($reportValues[$i]) $($reportValues[$i+1]). IsAGoodReport: $isAGoodReport"
+    }
+    else
+    {        
+        $isAGoodReport = ( ( [int]$reportValues[$i] -gt [int]$reportValues[$i+1] ) -and ( ( ( [int]$reportValues[$i] - [int]$reportValues[$i+1] ) -ge 1 ) -and ( ( [int]$reportValues[$i] - [int]$reportValues[$i+1] ) -le 3 ) ) )
+        # Write-Host "Descending. Current offset $i . Values $($reportValues[$i]) $($reportValues[$i+1]). IsAGoodReport: $isAGoodReport"
+    }
+
+    if((-not $isAGoodReport) -and (-not $wasDampened))
+    {
+      # So we're going to just do a recursive call with $i and $i+1 removed and isDampened $true
+      [string]$currentValueRemoved = ($reportValues[0 .. ($i-1)] + $reportValues[($i+1) .. ($reportValues.Count)]) -join ' '
+      [string]$nextValueRemoved = ($reportValues[0 .. ($i)] + $reportValues[($i+2) .. ($reportValues.Count)]) -join ' '
+      
+      $isAGoodReport = IsReportSafeWithDampening($currentValueRemoved, $true) -and IsReportSafeWithDampening($nextValueRemoved, $true)
+    }
+  }
+
+  $isAGoodReport
+}
+
+$puzzleInput = Get-Content .\originalinput.txt
+
+# For part 1, the criteria for any given line is "must be either all desc or asc" && "diff between 2 values must be 1 .. 3"
+$goodReport = 0
+$goodDampenedReport = 0
+
+foreach($report in $puzzleInput)
+{
+
+  if(IsReportSafe -report $report)
   {
     $goodReport++
   }
 
-  # As constructed, had to just run the loop again with a different short circuit.
-  for ($i = 0; ($i -lt $reportValues.Count - 1) -and ((-not $isAGoodReport) -and ($DampenedCounter -le 1)); $i++) 
-  {    
-    if($isAscending)
-    {        
-        $isAGoodReport = ( ( [int]$reportValues[$i] -lt [int]$reportValues[$i+1] ) -and ( ( ( [int]$reportValues[$i+1] - [int]$reportValues[$i] ) -ge 1 ) -and ( ( [int]$reportValues[$i+1] - [int]$reportValues[$i] ) -le 3 ) ) )                
-    }
-    else
-    {
-        $isAGoodReport = ( ( [int]$reportValues[$i] -gt [int]$reportValues[$i+1] ) -and ( ( ( [int]$reportValues[$i] - [int]$reportValues[$i+1] ) -ge 1 ) -and ( ( [int]$reportValues[$i] - [int]$reportValues[$i+1] ) -le 3 ) ) )        
-    }
-
-    if(-not $isAGoodReport)
-    {
-        $DampenedCounter++
-    }
-    
-    if($isAGoodReport)
-    {
-        $goodDampenedReport++
-    }
-    elseif((-not $isAGoodReport) -and ($DampenedCounter -le 1))
-    {
-        $goodDampenedReport++
-    }    
+  if(IsReportSafeWithDampening -report $report)
+  {
+    $goodDampenedReport++
   }
-
+  
 }
   
 Write-Host "Number of Good reports is: $goodReport"
